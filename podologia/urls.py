5e112e193fb5714ec4e1478aa77d8ea5df515ff7
Willian <willianjohny05@gmail.com>
2024-11-27 19:29:13 -0300

    View de Listar Podologos

--- podologia/urls.py
+++ podologia/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 # Padrões de URL para o projeto
 urlpatterns = [
-    path('admin/', admin.site.urls),  # URL para a área de administração do Django
+    path('admin/', admin.site.urls),
+        # URL para a área de administração do Django
     # path('', include('core.urls')),  # Inclui as URLs do app core, incluindo a página inicial
 
 ]
